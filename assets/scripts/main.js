37341083c0cc451cbf100b1acc24e6e2561b0ca0
Paul Csapak <office@webcascade.com>
2016-03-04 22:42:21 +0100

    DNA2Visual V0.1 working

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,34 +7,23 @@
     // An object literal
     var DNA = {
         dnaString: '',
-        squareRoot: 200,
+        squareRoot: 250,
         sideLength: 800,
         numBase: 4,
+        canvas: document.getElementById('dnaDisplay'),
      
         convertStringToArray: function(string) {
             console.time('convert');
-            console.log(DNA.numBase);
             var baseValue = parseInt(DNA.numBase);
             var rootSquared = DNA.squareRoot * DNA.squareRoot;
             var numOfPixels = rootSquared * baseValue * 3;
-            var cleanString = DNA.convertToBase4(string.trim().slice(0, numOfPixels));
-            // var re = new RegExp('.{1,' + bit + '}', 'g');
-            // var returnArray = cleanString.match(re);
-            var returnArray = [];
-            var tempString = '';
-            
-            var rgbChunk = [];
-            for (i = 1; i <= rootSquared; i++) {
-              var rgb = DNA.convertValueToRgb(cleanString.substr(baseValue * i, baseValue));
-              returnArray.push(rgb);
-              // rgbChunk.push(rgb);
-              // if (i % 3 === 0) {
-              //   returnArray.push(rgbChunk);
-              //   rgbChunk = [];
-              // }
-            }
+            var cleanString = string.trim().slice(0, numOfPixels);
+                // if (cleanString.length < numOfPixels)
+                cleanString = DNA.convertToBase4(cleanString);
 
-            console.timeEnd('convert');
+            var re = new RegExp('.{1,' + baseValue + '}', 'g');
+            var returnArray = cleanString.match(re);
+            
             console.log(returnArray);
             return returnArray;
         },
@@ -56,15 +45,14 @@
           return numberString;
         },
 
-        stringToNumbers: function(string) {
-          return string;
-        },
-
         init: function( settings ) {
           // set settings
           DNA.settings = settings;
 
           var form = $('#dnaForm');
+          var displayContainer = $('#dnaDisplayContainer');
+          var downloadButton = $('#downloadButton');
+          var canvas = $('#dnaDisplay');
 
           form.on('submit', function(e) {
             e.preventDefault();
@@ -74,8 +62,20 @@
 
             var rgbArray = DNA.convertStringToArray(result[1].value);
 
-            // DNA.executeD3(rgbArray);
+            DNA.executeD3(rgbArray);
+
+            displayContainer.slideDown(250);
           });
+
+          // when download button is clicked, provide PNG
+          downloadButton.on('click', function(e) {
+            var downloadStream = DNA.canvas.toDataURL('image/png');
+            // Change MIME type to trick the browser to downlaod the file instead of displaying it and define HTTP-style headers
+            downloadStream = downloadStream.replace(/^data:image\/[^;]*/, 'data:application/octet-stream');
+            downloadStream = downloadStream.replace(/^data:application\/octet-stream/, 'data:application/octet-stream;headers=Content-Disposition%3A%20attachment%3B%20filename=Canvas.png');
+            downloadButton.attr('href', downloadStream);
+          });
+
         },
      
         readSettings: function() {
@@ -83,52 +83,24 @@
         },
 
         executeD3: function(rgbArray) {
+          var ctx = DNA.canvas.getContext('2d');
+          var squareRoot = DNA.squareRoot;
+          var pixelSize = DNA.sideLength / squareRoot;
+          var k = 0;
+          for (var i=0;i<squareRoot;i++) {
+            for (var j=0;j<squareRoot;j++) {
+              ctx.fillStyle = 'rgb(' + 
+                               DNA.convertValueToRgb(rgbArray[k]) + ',' +
+                               DNA.convertValueToRgb(rgbArray[k+1]) + ',' +
+                               DNA.convertValueToRgb(rgbArray[k+2]) + ')';
 
-          var 
-            n = DNA.squareRoot, // square root of number of nodes
-            m = n * n, // number of data
-            d = 1, // dimension of data 
-            sen = 10, //size of each node (pixel)
-            nodes = [],
-            data = []
-          ; 
-           
-          // initialize nodes
-          for(var i = 0; i < n * n; i++){
-            nodes.push({
-              x: i % n,
-              y: Math.floor(i / n),
-              value: rgbArray[i]
-            });
+              ctx.fillRect(j*pixelSize, i*pixelSize, pixelSize, pixelSize);
+
+              k = k+3;
+            }
           }
-           
-          function rgb(array){
-            console.log(array);
-            // return 'rgb('+ array.map(function(r){return Math.round(r);}).join(',') +')';
-          }
-           
-          var 
-            svg = d3.select('#dnaDisplay').append('svg').attr('width', 800).attr('height', 800),
-            margin = 30,
-            width = n * sen,
-            height = n * sen
-          ;
-           
-          var 
-            rgb_nodes = svg.append('g').attr('class','nodes all');
-           
-          rgb_nodes
-            .selectAll('rect')
-            .data(nodes)
-            .enter().append('rect')
-              .attr('x', function(node){return node.x * sen;})
-              .attr('y', function(node){return node.y * sen;})
-              .attr('width', sen)
-              .attr('height', sen)
-              .style('fill', function(node){return rgb(node.value);})
-           
-           
-           
+
+          console.timeEnd('convert');
         }
     };
 
