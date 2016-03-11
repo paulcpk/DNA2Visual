dcf083acd1bb4c6a9bc9dfce1e31bb7a5b403a5f
Paul Csapak <office@webcascade.com>
2016-03-11 23:14:00 +0100

    created new color mode, persisting issue with download size

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -10,6 +10,7 @@
         squareRoot: 200,
         sideLength: 800,
         maxLength: 3000000,
+        mode: 'base-4',
         numBase: 4,
         canvas: document.getElementById('dnaDisplay'),
 
@@ -22,6 +23,9 @@
           var downloadButton = $('#downloadButton');
           var canvas = $('#dnaDisplay');
           var introduction = $('#introduction');
+          var exampleLinks = $('.example-link');
+          var examplesModal = $('#examplesModal');
+          var aminoacidTypes = $('#aminoacidTypes');
 
           form.on('submit', function(e) {
             e.preventDefault();
@@ -30,11 +34,23 @@
 
             // if result is valid 
             if (result[1].value.length > 0) {
+
               introduction.hide();
               form.find('textarea').removeClass('error');
-              DNA.numBase = parseInt(result[0].value);
+
+              DNA.mode = result[0].value;
+              DNA.numBase = result[0].value === 'base-4' ? 4 : 3;
+
               var rgbArray = DNA.convertStringToArray(result[1].value);
-              DNA.executeD3(rgbArray);
+
+              if (DNA.mode === 'base-4') {
+                aminoacidTypes.hide();
+                DNA.executeD3Color(rgbArray);
+              } else {
+                aminoacidTypes.show();
+                DNA.executeD3Codons(rgbArray);
+              }
+
               displayContainer.show();
             } else {
               displayContainer.hide();
@@ -46,32 +62,31 @@
 
           // when download button is clicked, provide PNG
           downloadButton.on('click', function(e) {
-            var downloadStream = DNA.canvas.toDataURL('image/png');
-            // Change MIME type to trick the browser to downlaod the file instead of displaying it and define HTTP-style headers
-            downloadStream = downloadStream.replace(/^data:image\/[^;]*/, 'data:application/octet-stream');
-            downloadStream = downloadStream.replace(/^data:application\/octet-stream/, 'data:application/octet-stream;headers=Content-Disposition%3A%20attachment%3B%20filename=Canvas.png');
-            downloadButton.attr('href', downloadStream);
+            // var downloadStream = DNA.canvas.toDataURL('image/png');
+            // // Change MIME type to trick the browser to downlaod the file instead of displaying it and define HTTP-style headers
+            // downloadStream = downloadStream.replace(/^data:image\/[^;]*/, 'data:application/octet-stream');
+            // downloadStream = downloadStream.replace(/^data:application\/octet-stream/, 'data:application/octet-stream;headers=Content-Disposition%3A%20attachment%3B%20filename=Canvas.png');
+            // downloadButton.attr('href', downloadStream);
+            ReImg.fromCanvas(DNA.canvas).downloadPng();
           });
 
-        },
+          // close example modale when user clicks link
+          exampleLinks.on('click', function(e) {
+            examplesModal.modal('hide');
+          });
 
-        readTextFile: function(file, callback) {
-          $.get(file, function(data) {
-             callback(data);
-          }, 'text');
         },
      
         convertStringToArray: function(string) {
             console.time('convert');
-            var cleanString = string.trim().toUpperCase().replace(/(\r\n|\n|\r)/gm,'');
-            console.log(cleanString);
-                cleanString = DNA.convertToBase4(cleanString).slice(0, DNA.maxLength);
+            var cleanString = string.trim().toUpperCase().replace(/(\r\n|\n|\r)/gm,'').slice(0, DNA.maxLength);
 
-            DNA.squareRoot = Math.floor(Math.sqrt(cleanString.length / DNA.numBase / 3));
-            console.log(cleanString.length);
-            console.log(DNA.squareRoot);
-            var rootSquared = DNA.squareRoot * DNA.squareRoot;
-            var numOfPixels = rootSquared * DNA.numBase * 3;
+            if (DNA.mode === 'base-4') {
+              cleanString = DNA.convertToNumbers(cleanString)
+              DNA.squareRoot = Math.floor(Math.sqrt(cleanString.length / DNA.numBase / 3));
+            } else {
+              DNA.squareRoot = Math.floor(Math.sqrt(cleanString.length / DNA.numBase));
+            }
 
             var re = new RegExp('.{1,' + DNA.numBase + '}', 'g');
             var returnArray = cleanString.match(re);
@@ -84,7 +99,80 @@
             return parseInt(string, DNA.numBase);
         },
 
-        convertToBase4: function(string) {
+        convertValueToCodon: function(string) {
+            var mapObj = {
+               TTT: '#FFE75F',
+               TTC: '#FFE75F',
+               TTA: '#FFE75F',
+               TTG: '#FFE75F',
+               CTT: '#FFE75F',
+               CTC: '#FFE75F',
+               CTA: '#FFE75F',
+               CTG: '#FFE75F',
+               ATT: '#FFE75F',
+               ATC: '#FFE75F',
+               ATA: '#FFE75F',
+               ATG: '#FFE75F',
+               GTT: '#FFE75F',
+               GTC: '#FFE75F',
+               GTA: '#FFE75F',
+               GTG: '#FFE75F',
+               CCT: '#FFE75F',
+               CCC: '#FFE75F',
+               CCA: '#FFE75F',
+               CCG: '#FFE75F',
+               GCT: '#FFE75F',
+               GCC: '#FFE75F',
+               GCA: '#FFE75F',
+               GCG: '#FFE75F',
+               TGG: '#FFE75F',
+               GGT: '#FFE75F',
+               GGC: '#FFE75F',
+               GGA: '#FFE75F',
+               GGG: '#FFE75F',
+               TCT: '#B3DEC0',
+               TCC: '#B3DEC0',
+               TCA: '#B3DEC0',
+               TCG: '#B3DEC0',
+               AGT: '#B3DEC0',
+               AGC: '#B3DEC0',
+               ACT: '#B3DEC0',
+               ACC: '#B3DEC0',
+               ACA: '#B3DEC0',
+               ACG: '#B3DEC0',
+               TAT: '#B3DEC0',
+               TAC: '#B3DEC0',
+               CAA: '#B3DEC0',
+               CAG: '#B3DEC0',
+               AAT: '#B3DEC0',
+               AAC: '#B3DEC0',
+               TGT: '#B3DEC0',
+               TGC: '#B3DEC0',
+               CAT: '#BBBFE0',
+               CAC: '#BBBFE0',
+               AAA: '#BBBFE0',
+               AAG: '#BBBFE0',
+               CGT: '#BBBFE0',
+               CGC: '#BBBFE0',
+               CGA: '#BBBFE0',
+               CGG: '#BBBFE0',
+               AGA: '#BBBFE0',
+               AGG: '#BBBFE0',
+               GAT: '#F8B7D3',
+               GAC: '#F8B7D3',
+               GAA: '#F8B7D3',
+               GAG: '#F8B7D3',
+               TAA: '#333333',
+               TGA: '#333333',
+               TAG: '#333333'
+            };
+            hexString = string.replace(/TTT|TTC|TTA|TTG|CTT|CTC|CTA|CTG|ATT|ATC|ATA|ATG|GTT|GTC|GTA|GTG|CCT|CCC|CCA|CCG|GCT|GCC|GCA|GCG|TGG|GGT|GGC|GGA|GGG|TCT|TCC|TCA|TCG|AGT|AGC|ACT|ACC|ACA|ACG|TAT|TAC|CAA|CAG|AAT|AAC|TGT|TGC|CAT|CAC|AAA|AAG|CGT|CGC|CGA|CGG|AGA|AGG|GAT|GAC|GAA|GAG|TAA|TGA|TAG/gi, function(matched){
+              return mapObj[matched];
+            });
+            return hexString;
+        },
+
+        convertToNumbers: function(string) {
           var mapObj = {
              A:"0",
              C:"1",
@@ -102,7 +190,7 @@
             console.log( DNA.settings );
         },
 
-        executeD3: function(rgbArray) {
+        executeD3Color: function(rgbArray) {
           var ctx = DNA.canvas.getContext('2d');
           var squareRoot = DNA.squareRoot;
           var pixelSize = DNA.sideLength / squareRoot;
@@ -121,7 +209,26 @@
           }
 
           console.timeEnd('convert');
+        },
+
+        executeD3Codons: function(rgbArray) {
+          var ctx = DNA.canvas.getContext('2d');
+          var squareRoot = DNA.squareRoot;
+          var pixelSize = DNA.sideLength / squareRoot;
+          var k = 0;
+          for (var i=0;i<squareRoot;i++) {
+            for (var j=0;j<squareRoot;j++) {
+              ctx.fillStyle = DNA.convertValueToCodon(rgbArray[k]);
+
+              ctx.fillRect(j*pixelSize, i*pixelSize, pixelSize, pixelSize);
+
+              k++;
+            }
+          }
+
+          console.timeEnd('convert');
         }
+        
     };
 
     DNA.init();
